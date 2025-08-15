3b520210ad99303407ed0ce55d3a84878ba34a19
Slonov Boris <sslonovborisss@gmail.com>
2025-08-15 20:02:25 +0700

    change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       cwd: "/var/www/vibro-laser/app/vl-land-ind",
       script: "npm",
       args: "start", // next start на 127.0.0.1:3000
-      env: { NODE_ENV: "production", PORT: "3001" },
+      env: { NODE_ENV: "production", PORT: "3003" },
       watch: false,
       //   max_restarts: 10
     },
