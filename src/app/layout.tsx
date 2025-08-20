af797a85abf165063c8afb2793f8337baf982f27
Slonov Boris <sslonovborisss@gmail.com>
2025-08-20 18:54:22 +0700

    fix m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import { Jost, Unbounded } from "next/font/google";
 import { COLORS_ROOT_STYLE } from "@/shared/config/colors";
 import { ModalProvider } from "./context/ModalContext";
+import Script from "next/script";
 
 const jost = Jost({ subsets: ["latin", "cyrillic"], display: "swap" });
 const unbounded = Unbounded({
@@ -33,22 +34,6 @@ export const metadata: Metadata = {
   },
 };
 
-const yandexMetricaScript = `
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function(m,e,t,r,i,k,a){
-        m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103738983', 'ym');
-
-    ym(103738983, 'init', {ssr:true, webvisor:true, clickmap:true, accurateTrackBounce:true, trackLinks:true});
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/103738983" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-`;
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -68,7 +53,38 @@ export default function RootLayout({
           position: "relative",
         }}
       >
-        <div dangerouslySetInnerHTML={{ __html: yandexMetricaScript }} />
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103738983', 'ym');
+
+              ym(103738983, 'init', {ssr:true, webvisor:true, clickmap:true, accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/103738983"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/103738983"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
         <ModalProvider>
           <Header />
           {children}
